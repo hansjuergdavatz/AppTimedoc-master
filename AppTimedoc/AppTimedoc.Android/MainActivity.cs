456254f7c1532eef44b29f9c08b54daa6e34c85a
hdavatz <hansjuerg.davatz@caprex.ch>
2018-01-05 08:11:05 +0100

    Splash Android

--- AppTimedoc/AppTimedoc.Android/MainActivity.cs
+++ AppTimedoc/AppTimedoc.Android/MainActivity.cs
@@ -9,19 +9,21 @@ using Android.OS;
 
 namespace AppTimedoc.Droid
 {
-    [Activity(Label = "AppTimedoc", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+  [Activity(Label = "AppTimedoc", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+  public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+  {
+    protected override void OnCreate(Bundle bundle)
     {
-        protected override void OnCreate(Bundle bundle)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+      TabLayoutResource = Resource.Layout.Tabbar;
+      ToolbarResource = Resource.Layout.Toolbar;
 
-            base.OnCreate(bundle);
+      base.SetTheme(Resource.Style.MainTheme);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
-            LoadApplication(new App());
-        }
+      base.OnCreate(bundle);
+
+      global::Xamarin.Forms.Forms.Init(this, bundle);
+      LoadApplication(new App());
     }
+  }
 }
 
